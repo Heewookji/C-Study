7ba1c9abbb5566f1594d8e9cb382ae5ad2ea1bef
heewookji <wlgmldnr@daum.net>
2020-05-27 21:50:01 +0900

    Add .gitignore file

---
+++ C-Study/C-Study/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Study
+//
+//  Created by Heewook on 2020/05/27.
+//  Copyright © 2020 Heewook. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
